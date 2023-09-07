8889e438249faca3ed9b9a8dc9860c3ef9397824
yeeydu <yeeysonduarte@gmail.com>
2023-09-07 15:40:01 +0100

    update postal to string

--- AddressesAPI/AddressesAPI/Models/Address.cs
+++ AddressesAPI/AddressesAPI/Models/Address.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string? Street { get; set; }
-        public int? Postal_code { get; set; }
+        public string? Postal_code { get; set; }
         public string? Parish { get; set; }
         public string? Council { get; set; }
         public string? District { get; set; }
