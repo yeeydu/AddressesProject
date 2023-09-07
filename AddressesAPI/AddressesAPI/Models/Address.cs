87101633514b4b8a8ba096cf187c21b63aa570a1
yeeydu <yeeysonduarte@gmail.com>
2023-09-07 15:22:05 +0100

    add model

---
+++ AddressesAPI/AddressesAPI/Models/Address.cs
@@ -0,0 +1,15 @@
+﻿namespace AddressesAPI.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string? Street { get; set; }
+        public int? Postal_code { get; set; }
+        public string? Parish { get; set; }
+        public string? Council { get; set; }
+        public string? District { get; set; }
+        public string? Country { get; set; }
+    }
+}
+
+
