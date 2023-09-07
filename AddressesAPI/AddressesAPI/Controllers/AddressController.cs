53bcdec718255bbb3df3d4072f98604b9d4f8a0b
yeeydu <yeeysonduarte@gmail.com>
2023-09-07 15:39:12 +0100

    add controller

---
+++ AddressesAPI/AddressesAPI/Controllers/AddressController.cs
@@ -0,0 +1,28 @@
+﻿global using AddressesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AddressesAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AddressController : ControllerBase
+    {
+        private static List<Address> addresses = new List<Address>()
+        {
+            new Address(),
+            new Address{Id=1, Street = "Eng. Duarte pacheco", Postal_code= 4575-234, Parish = "Torrao",Council= "MCN", District = "Porto", Country = "Portugal"}
+        };
+
+        [HttpGet("GetAll")]
+        public ActionResult<List<Address>> Get()
+        {
+            return Ok(addresses);
+        }
+
+        [HttpGet("Single")]
+        public ActionResult<List<Address>> GetSingle()
+        {
+            return Ok(addresses[1]);
+        }
+    }
+}
