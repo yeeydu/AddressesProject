311ae1ad0613ec5db22cc0a75e7fce52c1855c44
yeeydu <yeeysonduarte@gmail.com>
2023-09-08 16:45:59 +0100

    add ConnectionString w context

--- AddressesAPI/AddressesAPI/Program.cs
+++ AddressesAPI/AddressesAPI/Program.cs
@@ -1,8 +1,12 @@
+using AddressesAPI.Data;
 using AddressesAPI.Services.AddressService;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Add Database ConnectionString 
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
