26023653118bda8c465bd18a0cadbbf5301d6804
yeeydu <yeeysonduarte@gmail.com>
2023-09-08 16:35:32 +0100

    add DataContext

---
+++ AddressesAPI/AddressesAPI/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿global using Microsoft.EntityFrameworkCore;
+
+namespace AddressesAPI.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+        // DBset Property // representation of the table EF
+        public DbSet<Address> Addresses => Set<Address>();
+    }
+}
